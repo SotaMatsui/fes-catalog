b550cb484477150cd0603248444152ce7fde0214
SotaMatsui <sotamatsui0216@gmail.com>
2022-09-04 21:23:51 +0900

    fixed head tag as display favicon

--- pages/_app.js
+++ pages/_app.js
@@ -14,15 +14,19 @@ function MyApp({ Component, pageProps }) {
   }, [router.asPath]);
   return getLayout(<>
     <Head>
-      <meta name="robots" content="noindex"/>
+      <meta name="robots" content="noindex" />
       <title>本所祭22</title>
-      <head prefix="og:http://ogp.me/ns#"/>
+      <head prefix="og:http://ogp.me/ns#" />
       <meta property="og:url" content="https://honjo.one/" />
       <meta property="og:type" content="website" />
       <meta property="og:title" content="本所祭22" />
       <meta property="og:description" content="彩れ、青春の一ページ。 - 2022年度本所高校文化祭デジタルパンフレット" />
       <meta property="og:site_name" content="本所祭22" />
       <meta property="og:image" content="https://honjo.one/ogp.png" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link rel="manifest" href="/site.webmanifest" />
     </Head>
     <Component {...pageProps} />
   </>
